114fcf3804a9970847cbe5bb9d0d2fb0866c87dd
tt <825489611@qq.com>
2019-02-16 17:55:44 +0800

    兼容：print兼容python3，以后代码都往python3方向写

--- get_index_hist_data.py
+++ get_index_hist_data.py
@@ -1,4 +1,9 @@
 #coding: utf-8
+
+'''
+按年获取指数数据，保存到data/index/目录
+'''
+
 import tushare as ts
 import sys
 import time
@@ -13,21 +18,21 @@ end_year = int(sys.argv[3])
 output_base_path = 'data/index/%s/' % code
 if not os.path.exists(output_base_path):
     os.mkdir(output_base_path)
-print 'output_base_path = ', output_base_path
+print('output_base_path = ', output_base_path)
 
 for year in range(start_year, end_year+1):
     # 获取今年的数据时，end不允许超过今天的日期，否则返回空
     start_date = '%s-01-01' % year
     end_date = '%s-12-31' % year if year < int(time.strftime('%Y')) else time.strftime('%Y-%m-%d')
-    print '[%s, %s]' % (start_date, end_date)
+    print('[%s, %s]' % (start_date, end_date))
 
     output_csv = os.path.join(output_base_path, '%s.csv.gz' % year)
 
     year_data = ts.get_h_data(code, start=start_date, end=end_date, index=True, pause=2)
     if len(year_data) > 0:
         year_data.to_csv(output_csv, compression='gzip')
-        print year, 'finish. save to %s' % output_csv
+        print(year, 'finish. save to %s' % output_csv)
         time.sleep(10)
     else:
-        print 'no %s data about %s' % (year, code)
+        print('no %s data about %s' % (year, code))
 
